efa14cf6ec89e910c022e41a53c211a476251b7b
Evgeniy Khlystov <hlystovea@gmail.com>
2024-05-24 15:00:29 +0700

    Add: env.example file

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,7 @@
+import multiprocessing
 from os import environ
 
-bind = environ.get('HOST', '0.0.0.0') + ':' + environ.get('PORT', '8080')
-workers = environ.get('WORKERS', 8)
+bind = environ.get('HOST', '0.0.0.0') + ':' + environ.get('PORT', '8000')
+workers = multiprocessing.cpu_count() * 2 + 1
 graceful_timeout = 300
+reload = True
